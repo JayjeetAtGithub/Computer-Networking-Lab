bc2c0d83207c24d861ead470c9e83fd27e199f56
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-03-02 18:27:18 +0530

    Remove useless comment

--- lab-3/threaded_server.c
+++ lab-3/threaded_server.c
@@ -120,7 +120,6 @@ int main(int argc, char **argv) {
 	for (int i = 0; i < MAX_CONN; i++) {
 		clilen = sizeof(cliaddrs[i]);
 
-		// failing in this accept statement.
 		connfds[i] = accept(sockfd, (struct sockaddr *)&cliaddrs[i], &clilen); 
 		if (connfds[i] < 0) { 
 			perror("server acccept failed...\n"); 
